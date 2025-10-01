adf631dbb42754756822b9aa572800786a6c1adb
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-10-01 14:31:50 +0200

    init dev

--- app/src/components/logoComponent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div class="flex items-center justify-center gap-2 mt-5">
-    <img src="../assets/images/logo.svg" alt="logo" width="32">
-    <p>TripShare</p>
-</div>
-</template>
-
-<style>
-</style>
--- app/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './assets/main.css';
-import './assets/css/fonts.css';
-import App from './App.vue'
-import router from '../router/index';
-import store from '../store/index';
-
-createApp(App).use(router).use(store).mount('#app')
