ae5f22f81d222b945eaf8efc7f0c2d806c8e8a7b
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-10-01 15:12:57 +0200

    init app dev

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,14 @@
+import ChartComponent from "./components/ChartComponent"
+
 function App() {
 
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <ChartComponent/>
+        </>
     )
 }
 
