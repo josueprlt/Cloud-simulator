b7d4366c35ce2af0e4fb139e9d414e9c9145da15
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-10-02 09:19:40 +0200

    update dashboard front

--- app/src/pages/SimulatorPage.jsx
+++ app/src/pages/SimulatorPage.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import FormComponent from "../components/form/AwsForm.jsx";
+/*import FormComponent from "../views/AwsForm.jsx";*/
 
 function HistoricPage() {
     return (
-        <div className="p-6 space-y-6">
-            <h1 className="text-2xl font-bold">Simulator place</h1>
-
-            <div className="flex justify-center">
-                <div className="w-1/2 bg-white p-4">
-                    <FormComponent/>
-                </div>
-            </div>
-        </div>
+        <p>form</p>
+        /*<FormComponent/>*/
     );
 }
 
