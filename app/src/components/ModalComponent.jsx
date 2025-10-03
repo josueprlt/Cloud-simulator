1b6e59c37191675a1c6dfac0d6dd13f7710c4bc8
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-10-03 10:15:52 +0200

    fix 2 style modal

--- app/src/components/ModalComponent.jsx
+++ app/src/components/ModalComponent.jsx
@@ -5,15 +5,15 @@ export default function ModalComponent({ isOpen, onClose, children }) {
 
     return (
         <div
-            className="fixed inset-0 bg-black/30 bg-opacity-50 flex justify-center items-center z-50"
+            className="fixed inset-0 bg-black/30 bg-opacity-50 flex justify-center items-center z-50 h-screen"
             onClick={onClose}
         >
             <div
-                className="bg-white rounded-2xl shadow-xl p-6 w-11/12 max-w-md relative animate-fadeIn"
+                className="bg-white rounded-2xl shadow-xl p-6 my-10 w-11/12 max-h-full overflow-auto max-w-md relative animate-fadeIn"
                 onClick={(e) => e.stopPropagation()}
             >
                 <button
-                    className="absolute top-3 right-3 text-gray-600 hover:text-gray-900 text-2xl"
+                    className="absolute top-3 right-3 text-gray-600 hover:text-gray-900 text-2xl cursor-pointer"
                     onClick={onClose}
                 >
                     &times;
